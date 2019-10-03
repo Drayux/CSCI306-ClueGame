ac4a7605fe74e29b94beaaaef34540d8d02908e1
William Dempsey <wdempsey@mines.edu>
2019-10-03 10:59:33 -0600

    Git is getting annoying

--- src/experiment/IntBoard.java
+++ src/experiment/IntBoard.java
@@ -1,31 +1,43 @@
 package experiment;
 
-import java.util.Map;
-import java.util.Set;
+import java.util.HashMap;
+import java.util.HashSet;
 
+//Class for a 4x4 test board
 public class IntBoard {
     
-    private BoardCell[] grid;
-    private Map<BoardCell, Set<BoardCell>> adjacencies;
-    private Set<BoardCell> targets;
+	public static final int BOARD_SIZE = 4;
+	
+    private BoardCell[] grid; 	//Row first, column second order
+    private HashMap<BoardCell, HashSet<BoardCell>> adjacencies;
+    private HashSet<BoardCell> targets;
     
     public IntBoard() {
+        grid = new BoardCell[BOARD_SIZE * BOARD_SIZE];
         
+        adjacencies = new HashMap<>();
+        calcAdjacencies();
         
     }
     
     public BoardCell getBoardCell(int r, int c) {
-        return null;
+        return grid[r * BOARD_SIZE + c];
         
     }
     
-    public Set<BoardCell> getAdjList() {
-        return null;
+    public HashSet<BoardCell> getAdjList(BoardCell c) {
+        return adjacencies.get(c);
         
     }
     
-    public Set<BoardCell> getTargets() {
-        return null;
+    public void calcTargets(BoardCell startCell, int pathLength) {
+        //Calculate all possible locations from a given square
+        //Store as set in targets
+        
+    }
+    
+    public HashSet<BoardCell> getTargets() {
+        return targets;
         
     }
     
@@ -34,12 +46,17 @@ public class IntBoard {
     private void calcAdjacencies() {
         //For every cell on the board, calculate a set of all adjacent cells
         //Store as map in adjacencies
-        
-    }
-    
-    private void calcTargets(BoardCell startCell, int pathLength) {
-        //Calculate all possible locations from a given square
-        //Store as set in targets
+    	for (int i = 0; i < BOARD_SIZE * BOARD_SIZE; i++) {
+    		HashSet<BoardCell> adj = new HashSet<>();
+    		
+    		if (i > 0) adj.add(grid[i - 1]);  //Check square to left
+    		if (i >= BOARD_SIZE) adj.add(grid[i - BOARD_SIZE]);  //Check square above
+    		if (i < BOARD_SIZE * BOARD_SIZE - 1) adj.add(grid[i + 1]);  //Check square to right
+    		if (i < BOARD_SIZE * (BOARD_SIZE - 1)) adj.add(grid[i + BOARD_SIZE]);  //Check square below
+    		
+    		adjacencies.put(grid[i], adj);
+    		
+    	}
         
     }
     
