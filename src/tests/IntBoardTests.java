eb90c294c9f8f0f14a3f2069d6d32667a3bac1be
Kevin OLinn <[kolinn@mymail.mines.edu]>
2019-10-07 13:08:27 -0600

    Tests are finished

--- src/tests/IntBoardTests.java
+++ src/tests/IntBoardTests.java
@@ -1,51 +1,135 @@
 package tests;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
-import java.util.HashSet;
+import java.util.Set;
 
 import org.junit.Before;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
 import experiment.BoardCell;
 import experiment.IntBoard;
 
-class IntBoardTests {
-	
-  private IntBoard board;
-
-
+public class IntBoardTests {
+	private IntBoard board;
 	@Before
     public void beforeAll() {
-       board = new IntBoard();  // constructor should call calcAdjacencies() so you can test them
+       board = new IntBoard();
     }
-	
-    /*
-	 * Test adjacencies for top left corner
+	/*
+	 * Test for top left corner
 	 */
 	@Test
-	public void testAdjacency0()
-	{
+	public void testAdjacency0_0() {
 		BoardCell cell = board.getBoardCell(0,0);
 		Set<BoardCell> testList = board.getAdjList(cell);
 		assertTrue(testList.contains(board.getBoardCell(1, 0)));
 		assertTrue(testList.contains(board.getBoardCell(0, 1)));
 		assertEquals(2, testList.size());
 	}
-	
+
+	/*
+	 * Test for bottom right corner
+	 */
 	@Test
-	public void testTargets0_3()
-	{
-		BoardCell cell = board.getBoardCell(0, 0);
-		board.calcTargets(cell, 3);
-		HashSet targets = board.getTargets();
-		assertEquals(6, targets.size());
-		assertTrue(targets.contains(board.getBoardCell(3, 0)));
-		assertTrue(targets.contains(board.getBoardCell(2, 1)));
-		assertTrue(targets.contains(board.getBoardCell(0, 1)));
-		assertTrue(targets.contains(board.getBoardCell(1, 2)));
-		assertTrue(targets.contains(board.getBoardCell(0, 3)));
-		assertTrue(targets.contains(board.getBoardCell(1, 0)));
+	public void testAdjacency3_3() {
+		BoardCell cell = board.getBoardCell(3,3);
+		Set<BoardCell> testList = board.getAdjList(cell);
+		assertTrue(testList.contains(board.getBoardCell(2, 3)));
+		assertTrue(testList.contains(board.getBoardCell(3, 2)));
+		assertEquals(2, testList.size());
 	}
+	/*
+	 * Test for right edge
+	 */
+	@Test
+	public void testAdjacency1_3() {
+		BoardCell cell = board.getBoardCell(1,3);
+		Set<BoardCell> testList = board.getAdjList(cell);
+		assertTrue(testList.contains(board.getBoardCell(0, 3)));
+		assertTrue(testList.contains(board.getBoardCell(2, 3)));
+		assertTrue(testList.contains(board.getBoardCell(1, 2)));
+		assertEquals(3, testList.size());
+	}
+	/*
+	 * Tests for Bottom left corner 
+	 */
+	@Test
+	public void testAdjacency3_0() {
+		BoardCell cell = board.getBoardCell(3,0);
+		Set<BoardCell> testList = board.getAdjList(cell);
+		assertTrue(testList.contains(board.getBoardCell(2, 0)));
+		assertTrue(testList.contains(board.getBoardCell(3, 1)));
+		assertEquals(2, testList.size());
+	}
+	/*
+	 * Test  for the middle board squares, (1,1) and (2,2)
+	 */
+	@Test
+	public void testAdjacency1_1() {
+		BoardCell cell = board.getBoardCell(1,1);
+		Set<BoardCell> testList = board.getAdjList(cell);
+		assertTrue(testList.contains(board.getBoardCell(0, 1)));
+		assertTrue(testList.contains(board.getBoardCell(2, 1)));
+		assertTrue(testList.contains(board.getBoardCell(1, 0)));
+		assertTrue(testList.contains(board.getBoardCell(1, 2)));
+		assertEquals(4, testList.size());
+	}
+	@Test
+	public void testAdjacency2_2() {
+		BoardCell cell = board.getBoardCell(2,2);
+		Set<BoardCell> testList = board.getAdjList(cell);
+		assertTrue(testList.contains(board.getBoardCell(2, 1)));
+		assertTrue(testList.contains(board.getBoardCell(1, 2)));
+		assertTrue(testList.contains(board.getBoardCell(2, 3)));
+		assertTrue(testList.contains(board.getBoardCell(3, 2)));
+		assertEquals(4, testList.size());
+	}
+	/*
+	 */
+	/*
+	 * Test (1,1) and a if you roll 3
+	 */
+	@Test
+	public void testTargets1_1_3()
+	{
+		BoardCell cell = board.getBoardCell(1, 1);
+		board.calcTargets(cell, 3);
+		Set targets = board.getTargets();
+	
+
+		assertTrue(targets.contains(board.getBoardCell(3, 0)));
+		assertTrue(targets.contains(board.getBoardCell(1, 0)));
+		assertTrue(targets.contains(board.getBoardCell(0, 1)));
+		assertTrue(targets.contains(board.getBoardCell(2, 1)));
+ 		assertTrue(targets.contains(board.getBoardCell(0, 3)));
+		assertTrue(targets.contains(board.getBoardCell(2, 3)));
+		assertTrue(targets.contains(board.getBoardCell(3, 2)));
+		assertEquals(8,targets.size());
+	}
+	
+
+	/*
+	 * tests where player is (2, 2) and if you roll of 2
+	 */
+	
+
+	@Test
+	public void testTargets2_2_2()
+	{
+		BoardCell cell = board.getBoardCell(2, 2);
+		board.calcTargets(cell, 2);
+		Set targets = board.getTargets();
+		assertEquals(6,targets.size());
+		assertTrue(targets.contains(board.getBoardCell(2, 0)));
+		assertTrue(targets.contains(board.getBoardCell(1, 1)));
+		assertTrue(targets.contains(board.getBoardCell(3, 1)));
+		assertTrue(targets.contains(board.getBoardCell(3, 3)));
+		assertTrue(targets.contains(board.getBoardCell(1, 3)));
+		assertTrue(targets.contains(board.getBoardCell(0, 2)));
+
+	}
+	
 
 }
