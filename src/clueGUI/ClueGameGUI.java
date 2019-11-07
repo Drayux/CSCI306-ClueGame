ed94e2b99b982fc78653ff1fb3c8b99c5185a422
Kevinolinn8 <kolinn@mymail.mines.edu>
2019-11-07 11:20:15 -0700

    added drop down menu

--- src/clueGUI/ClueGameGUI.java
+++ src/clueGUI/ClueGameGUI.java
@@ -2,8 +2,13 @@ package clueGUI;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 
 
@@ -20,6 +25,9 @@ public class ClueGameGUI extends JFrame {
 		setTitle("Clue Game"); // set title to generic "clue game"
 		CPLayout();
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		JMenuBar menuBar = new JMenuBar();
+		setJMenuBar(menuBar);
+		menuBar.add(createFileMenu());
 
 	}
 
@@ -29,6 +37,28 @@ public class ClueGameGUI extends JFrame {
 		JPanel control = new ControlPanel();
 		add(control, BorderLayout.SOUTH);
 
+	}
+	
+	
+	private JMenu createFileMenu()
+	{
+		JMenu menu = new JMenu("File"); 
+		menu.add(createFileExitItem());
+		return menu;
+	}
+
+	private JMenuItem createFileExitItem()
+	{
+		JMenuItem item = new JMenuItem("Exit");
+		class MenuItemListener implements ActionListener {
+			public void actionPerformed(ActionEvent e)
+			{
+				System.exit(0);
+			}
+		}
+		item.addActionListener(new MenuItemListener());
+
+		return item;
 	}
 
 
---
+++ src/clueGUI/DetectiveNotesGUI.java
@@ -0,0 +1,5 @@
+package clueGUI;
+
+public class DetectiveNotesGUI {
+
+}
