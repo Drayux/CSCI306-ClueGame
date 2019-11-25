6001cf2b618f2997bcb3db4959ad19ebfa7ea470
William Dempsey <wdempsey@mines.edu>
2019-11-24 18:12:10 -0700

    Board Display improvements

--- src/clueGUI/BoardDisplay.java
+++ src/clueGUI/BoardDisplay.java
@@ -27,41 +27,50 @@ public class BoardDisplay extends JPanel {
 
 	private int cellSize = 32;
 	private int doorSize = 8;
-	
+
 	private static final Color walkwayColor = new Color(237, 211, 137, 255);
 	private static final Color selectedWalkwayColor = new Color(250, 234, 190, 255);
-	private static final Color selectedRoomColor = new Color(240, 237, 235, 255);
+	private static final Color selectedRoomColor = new Color(245, 245, 235, 255);
 	private static final Color borderColor = new Color(140, 140, 120, 255);
 	private static final Color playerBorderColor = new Color(90, 90, 60, 255);
 	private static final Color textColor = new Color(75, 75, 40, 255);
 	private static final Color doorColor = new Color(136, 86, 15, 255);
 	private static final Color backgroundColor = new Color(220, 215, 210, 255);
 
-	private Map<Character, Dimension> roomCenters = new HashMap<Character, Dimension>();
+	//private Map<Character, Dimension> roomCenters = new HashMap<Character, Dimension>();
+	private Set<BoardRoom> rooms = new HashSet<BoardRoom>();
 	private Dimension lastClick = null;
-	
+
 	public final ClickListener listener = new ClickListener();
-	
+
 	public BoardDisplay() {
 		this.addComponentListener(new ResizeListener());
 		//this.addMouseListener(new ClickListener());
-		
+
 		for (Card gameCard : Board.getInstance().getGameCards()) {
 			char initial = 0;
-			
+			String name = null;
+
 			if (gameCard.getType() == CardType.ROOM) {
 				for (char c : Board.getInstance().getLegend().keySet()) {
 					if (Board.getInstance().getLegend().get(c).equals(gameCard.getName())) {
 						initial = c;
-						
+						name = gameCard.getName();
+
 						break;
 					}
 				}
-				
-				if (initial != 0) roomCenters.put(initial, calculateRoomCenter(Board.getInstance(), initial));
-				
+
+				if (initial != 0 && name != null) {
+					BoardRoom room = new BoardRoom(initial, name);
+					room.defineRoomCenter(calculateRoomCenter(Board.getInstance(), initial));
+
+					rooms.add(room);
+				}
 			}
-			
+
+			this.repaint();
+
 		}
 	}
 
@@ -70,29 +79,43 @@ public class BoardDisplay extends JPanel {
 		super.paintComponent(g);
 
 		BoardCell curCell = null;
-		
+
 		// Draw the background
 		g.setColor(backgroundColor);
 		g.fillRect(0, 0, (int) getPreferredSize().getHeight() + 1, (int) getPreferredSize().getWidth() + 1);
+
+		// Compile a list of selected room chars
+		Set<Character> selectedRooms = new HashSet<Character>();
+		for (int i = 0; i < Board.getInstance().getNumRows(); i++) {
+			for (int j = 0; j < Board.getInstance().getNumColumns(); j++) {
+				if (Board.getInstance().getCellAt(i, j).isSelected) selectedRooms.add(Board.getInstance().getCellAt(i, j).getInitial());
+				
+			}
+		}
+		
+		// Draw the selected rooms
+		for (Character c : selectedRooms) {
+			for (BoardRoom room : rooms) {
+				if (room.getInitial() == c) {
+					room.drawSelected(g, cellSize, selectedRoomColor);
+					break;
+					
+				}
+			}
+		}
 		
 		// Draw the board
 		for (int i = 0; i < Board.getInstance().getNumRows(); i++) {
 			for (int j = 0; j < Board.getInstance().getNumColumns(); j++) {
-				
+
 				curCell = Board.getInstance().getCellAt(i, j);
-				
-				if (curCell.isSelected && !curCell.isWalkway()) {
-					g.setColor(selectedRoomColor);
-					g.fillRect(j * cellSize, i * cellSize, cellSize, cellSize);
-					
-				}
-				
+
+				// Draw the walkways
 				g.setColor(doorColor);
 				switch (curCell.getDoorDirection()) {
 				case NONE:
-					// Up here add full room coloring for selection
 					if (!curCell.isWalkway()) break;
-					
+
 					// Draw the board cell
 					g.setColor((curCell.isSelected) ? selectedWalkwayColor : walkwayColor);
 					g.fillRect(j * cellSize, i * cellSize, cellSize, cellSize);
@@ -100,51 +123,65 @@ public class BoardDisplay extends JPanel {
 					// Draw the border
 					g.setColor(borderColor);
 					g.drawRect(j * cellSize, i * cellSize, cellSize, cellSize);
-					
+
 					break;
-					
+
 				case UP:
 					g.fillRect(j * cellSize, i * cellSize, cellSize, doorSize);
 					break;
-					
+
 				case RIGHT:
 					g.fillRect((j * cellSize) + (cellSize - doorSize), i * cellSize, doorSize, cellSize);
 					break;
-				
+
 				case DOWN:
 					g.fillRect(j * cellSize, (i * cellSize) + (cellSize - doorSize), cellSize, doorSize);
 					break;
-					
+
 				case LEFT:
 					g.fillRect(j * cellSize, i * cellSize, doorSize, cellSize);
 					break;
-					
+
 				default:
 					break;
+
+				}
+			}
+		}
+
+		// Draw the players (on walkways)
+		for (int i = 0; i < Board.MAX_PLAYERS_COUNT; i++) {
+			if (Board.getInstance().getPlayer(i).getLocation().isWalkway()) {
+				g.setColor(Board.getInstance().getPlayer(i).getColor());
+				g.fillOval(Board.getInstance().getPlayer(i).getColumn() * cellSize + 1, Board.getInstance().getPlayer(i).getRow() * cellSize + 1, cellSize - 2, cellSize - 2);
+
+				g.setColor(playerBorderColor);
+				g.drawOval(Board.getInstance().getPlayer(i).getColumn() * cellSize + 1, Board.getInstance().getPlayer(i).getRow() * cellSize + 1, cellSize - 2, cellSize - 2);
+
+			} else {
+				for (BoardRoom room : rooms) {
+					if (room.getInitial() == Board.getInstance().getPlayer(i).getLocation().getInitial()) room.putPlayer(Board.getInstance().getPlayer(i));
 					
 				}
 			}
 		}
-		
-		// Draw the players
-		for (int i = 0; i < Board.MAX_PLAYERS_COUNT; i++) {
-			g.setColor(Board.getInstance().getPlayer(i).getColor());
-			g.fillOval(Board.getInstance().getPlayer(i).getColumn() * cellSize + 1, Board.getInstance().getPlayer(i).getRow() * cellSize + 1, cellSize - 2, cellSize - 2);
-			
-			g.setColor(playerBorderColor);
-			g.drawOval(Board.getInstance().getPlayer(i).getColumn() * cellSize + 1, Board.getInstance().getPlayer(i).getRow() * cellSize + 1, cellSize - 2, cellSize - 2);
+
+		// Draw the players (in rooms)
+		for (BoardRoom room : rooms) {
+			room.drawPlayers(g, cellSize, playerBorderColor);
+			room.clearPlayers();
 			
 		}
-		
+
 		// Draw the room labels
 		g.setColor(textColor);
-		
-		for (char initial : roomCenters.keySet()) {
-			int strWidth = g.getFontMetrics().stringWidth(Board.getInstance().getLegend().get(initial));
+
+		for (BoardRoom room : rooms) {
+			int strWidth = g.getFontMetrics().stringWidth(room.getLabel());
 			int strHeight = g.getFontMetrics().getHeight();
-			
-			g.drawString(Board.getInstance().getLegend().get(initial), (int) (roomCenters.get(initial).getWidth() * cellSize - (strWidth / 2)), (int) (roomCenters.get(initial).getHeight() * cellSize) + (strHeight / 2));
-			
+
+			g.drawString(room.getLabel(), (int) (room.getCenterX() * cellSize - (strWidth / 2)), (int) (room.getCenterY() * cellSize) + (strHeight / 2));
+
 		}
 	}
 
@@ -156,61 +193,61 @@ public class BoardDisplay extends JPanel {
 
 	public BoardCell getCellAtClick() {
 		if (lastClick == null) return null;
-		
+
 		BoardCell location = Board.getInstance().getCellAt((int) lastClick.getHeight() / cellSize, (int) lastClick.getWidth() / cellSize);
 		return location; 
 	}
-	
+
 	public void calculateCellSize(int numRows, int numColumns) {
 		double maxHeight = this.getHeight() / numRows;
 		double maxWidth = this.getWidth() / numColumns;
-		
+
 		cellSize = (maxHeight > maxWidth) ? (int) maxWidth : (int) maxHeight;
 		doorSize = cellSize / 4;
-		
+
 	}
-	
+
 	private class ResizeListener extends ComponentAdapter {
-		
+
 		@Override
 		public void componentResized(ComponentEvent e) {
 			calculateCellSize(Board.getInstance().getNumRows(), Board.getInstance().getNumColumns());
 			BoardDisplay.this.repaint();
-			
+
 		}
 	}
-	
+
 	public class ClickListener implements MouseListener {
 
 		@Override
 		public void mouseClicked(MouseEvent e) {
 			lastClick = new Dimension(e.getX(), e.getY());
 			//System.out.println("Clicked: " + e.getX() + ", " + e.getY());
-			
+
 			//Board.getInstance().calcTargets(Board.getInstance().getPlayer(Board.getInstance().getHumanPlayer()).getLocation(), Board.getInstance().getRoll());
 			BoardCell selection = Board.getInstance().getPlayer(Board.getInstance().getHumanPlayer()).pickLocation(Board.getInstance().getTargets());
-			
+
 			if (selection == null) {
 				JOptionPane.showMessageDialog(null, "Invalid location selected!", "Error", JOptionPane.INFORMATION_MESSAGE );
 				return;
-				
+
 			}
-			
+
 			for (BoardCell cell : Board.getInstance().getTargets()) cell.isSelected = false;
 			Board.getInstance().getPlayer(Board.getInstance().getHumanPlayer()).move(selection);
 			Board.getInstance().getGUI().repaint();
-			
+
 			BoardDisplay.this.removeMouseListener(listener);
-			
+
 			// Handle human player suggestions
 			System.out.println("TODO handle human suggestions");
-			
+
 			Board.getInstance().getGUI().getControlPanel().updateFields();
 			Board.getInstance().getGUI().getControlPanel().enableNextPlayerButton();
-			
+
 			/*synchronized(WaitThread.monitor) {
 				WaitThread.monitor.notify(); 
-				
+
 			}*/
 		}
 
@@ -225,131 +262,131 @@ public class BoardDisplay extends JPanel {
 
 		@Override
 		public void mouseExited(MouseEvent e) {}
-		
+
 	}
-	
+
 	private static Dimension calculateRoomCenter(Board board, char roomInitial) {
 		int maxColTop = -1;
 		int maxColBottom = -1;
-		
+
 		int curColTop = -1;
 		int curColBottom = -1;
-		
+
 		int shiftCountRight = 0;
 		int shiftCountLeft = 0;
-		
+
 		int maxArea = 0;
 		int curArea = 0;
-		
+
 		int maxCoordX = 0;
 		int maxCoordY = 0;
-		
+
 		int maxDimX = 0;
 		int maxDimY = 0;
-		
+
 		// In every column, find the longest section of room
 		for (int j = 0; j < board.getNumColumns(); j++) {
 			for (int i = 0; i < board.getNumRows(); i++) {
 				if (board.getCellAt(i, j).getInitial() == roomInitial) {
 					if (curColTop == -1) curColTop = i;
 					curColBottom = i;
-					
+
 				} else if (curColTop != -1) {
 					if ((curColBottom - curColTop) > (maxColBottom - maxColTop)) {
 						maxColTop = curColTop;
 						maxColBottom = curColBottom;
-						
+
 					}
-					
+
 					curColTop = -1;
 					curColBottom = -1;
-					
+
 				}
-				
+
 			}
-			
+
 			// Finally once the board ends, the room also has if not already
 			if ((curColBottom - curColTop) > (maxColBottom - maxColTop)) {
 				maxColTop = curColTop;
 				maxColBottom = curColBottom;
-				
+
 			}
-			
+
 			curColTop = -1;
 			curColBottom = -1;
-			
+
 			// Test output
 			//System.out.println("Top: " + maxColTop);
 			//System.out.println("Bottom: " + maxColBottom);
 			//System.out.println();
-			
+
 			///////////////////////////////////////////
-			
+
 			shiftCountRight = 0;
 			shiftCountLeft = 0;
-			
+
 			boolean edge = false;
-			
+
 			if ((maxColBottom - maxColTop) > 0) {
 				// If a room is present, check for the same row as far right as possible
 				for (int k = j + 1; k < board.getNumColumns() && !edge; k++) {
 					for (int l = maxColTop; l <= maxColBottom; l++) {
 						if (board.getCellAt(l, k).getInitial() != roomInitial) {
 							edge = true;
-							
+
 							break;
 						}
 					}
-					
+
 					shiftCountRight = (edge) ? shiftCountRight : (shiftCountRight + 1);
-					
+
 				}
-				
+
 				edge = false;
-				
+
 				// If a room is present, check for the same row as far left as possible
 				for (int k = j - 1; k >= 0 && !edge; k--) {
 					for (int l = maxColTop; l <= maxColBottom; l++) {
 						if (board.getCellAt(l, k).getInitial() != roomInitial) {
 							edge = true;
-							
+
 							break;
 						}
 					}
-					
+
 					shiftCountLeft = (edge) ? shiftCountLeft : (shiftCountLeft + 1);
-					
+
 				}
 			}
-			
+
 			curArea = (shiftCountRight + shiftCountLeft + 1) * (maxColBottom - maxColTop + 1);
-			
+
 			if (curArea >= maxArea) {	// >= Will prioritize longer rooms over taller rooms given the same size (Look at room S on our layout for reference)
 				maxArea = curArea;
-				
+
 				maxCoordX = j - shiftCountLeft;
 				maxCoordY = maxColTop;
-				
+
 				maxDimX = shiftCountRight + shiftCountLeft + 1;
 				maxDimY = maxColBottom - maxColTop + 1;
-				
+
 			}
-			
+
 			maxColTop = -1;
 			maxColBottom = -1;
-			
+
 		}
-		
+
 		//Test output
-		//System.out.println("Coord X: " + maxCoordX);
-		//System.out.println("Coord Y: " + maxCoordY);
-		
-		//System.out.println("Dim X: " + maxDimX);
-		//System.out.println("Dim Y: " + maxDimY);
-		
+		//		System.out.println("Coord X: " + maxCoordX);
+		//		System.out.println("Coord Y: " + maxCoordY);
+		//		
+		//		System.out.println("Dim X: " + maxDimX);
+		//		System.out.println("Dim Y: " + maxDimY);
+
 		return new Dimension(maxCoordX + (maxDimX / 2), maxCoordY + (maxDimY / 2));
 	}
-	
+
 	public Dimension getLastClick() { return lastClick; }
-	
+
 }
--- src/clueGUI/BoardRoom.java
+++ src/clueGUI/BoardRoom.java
@@ -1,9 +1,12 @@
 package clueGUI;
 
+import clueGame.Board;
 import clueGame.BoardCell;
 import clueGame.Player;
 
+import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Graphics;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
@@ -14,54 +17,110 @@ import java.util.Set;
  *	From this, we can easily place players in the room, and color
  *		the entire room a certain color if it is a valid location.
  */
+
 public class BoardRoom {
-	
+
 	private char initial;
 	private String label = null;
-	
-	private float centerX;
-	private float centerY;
-	
+
+	private double centerX;
+	private double centerY;
+
 	private Map<BoardCell, Player> roomCells = null;
-	
+
 	public BoardRoom(char c, String l) {
 		initial = c;
 		label = l;
-		
+
 		roomCells = new HashMap<BoardCell, Player>();
-		
+		defineRoom(Board.getInstance());
+
 	}
-	
-	public void defineRoom(Set<BoardCell> cells) {
-		for (BoardCell cell : cells) roomCells.put(cell, null);
-		
-	}
-	
-	public void putPlayer(Player player) {
-		// Create a set of valid locations
-		Set<BoardCell> locations = new HashSet<BoardCell>();
-		
-		for (BoardCell cell : roomCells.keySet()) {
-			if (roomCells.get(cell) == null && !cell.isDoorway()) locations.add(cell);
-			
-		}
-		
-		int rand = (int) Math.random() * locations.size(); 
-		
-		for (BoardCell cell : locations) {
-			if (rand == 0) roomCells.put(cell, player);
-			else rand--;
-			
-		}
-	}
-	
-	public void removePlayer(Player player) {
-		for (BoardCell cell : roomCells.keySet()) {
-			if (roomCells.get(cell) == player) {
-				roomCells.put(cell, null);
-				
-				return;
+
+	//	public void defineRoom(Set<BoardCell> cells) {
+	//		for (BoardCell cell : cells) roomCells.put(cell, null);
+	//		
+	//	}
+
+	// Helper function
+	public void defineRoom(Board board) {
+		for (int j = 0; j < board.getNumColumns(); j++) {
+			for (int i = 0; i < board.getNumRows(); i++) {
+				if (board.getCellAt(i, j).getInitial() == initial) roomCells.put(board.getCellAt(i, j), null);
+
 			}
 		}
 	}
+
+	public void putPlayer(Player player) {
+		// Create a set of valid locations
+		Set<BoardCell> locations = new HashSet<BoardCell>();
+
+		for (BoardCell cell : roomCells.keySet()) {
+			if (roomCells.get(cell) == null && !cell.isDoorway()) locations.add(cell);
+
+		}
+
+		int rand = (int) Math.random() * locations.size(); 
+
+		for (BoardCell cell : locations) {
+			if (rand == 0) {
+				roomCells.put(cell, player);
+				
+				return;
+			}
+			else rand--;
+
+		}
+	}
+
+	//	public void removePlayer(Player player) {
+	//		for (BoardCell cell : roomCells.keySet()) {
+	//			if (roomCells.get(cell) == player) {
+	//				roomCells.put(cell, null);
+	//				
+	//				return;
+	//			}
+	//		}
+	//	}
+
+	public void clearPlayers() {
+		for (BoardCell cell : roomCells.keySet()) {
+			if (roomCells.get(cell) != null) roomCells.put(cell, null);
+
+		}
+	}
+
+	public void drawSelected(Graphics g, int cellSize, Color color) {
+		g.setColor(color);
+		
+		for (BoardCell cell : roomCells.keySet()) g.fillRect(cell.getColumn() * cellSize, cell.getRow() * cellSize, cellSize, cellSize);
+	}
+	
+	public void drawPlayers(Graphics g, int cellSize, Color borderColor) {
+		for (BoardCell cell : roomCells.keySet()) {
+			Player player = roomCells.get(cell);
+
+			if (player != null) {
+				g.setColor(player.getColor());
+				g.fillOval(cell.getColumn() * cellSize + 1, cell.getRow() * cellSize + 1, cellSize - 2, cellSize - 2);
+	
+				g.setColor(borderColor);
+				g.drawOval(cell.getColumn() * cellSize + 1, cell.getRow() * cellSize + 1, cellSize - 2, cellSize - 2);
+
+			}
+		}
+	}
+
+	public void defineRoomCenter(Dimension center) {
+		this.centerX = center.getWidth();
+		this.centerY = center.getHeight();
+
+	}
+
+	public double getCenterX() { return centerX; }
+	public double getCenterY() { return centerY; }
+	public String getLabel() { return label; }
+	public char getInitial() { return initial; }
+
 }
